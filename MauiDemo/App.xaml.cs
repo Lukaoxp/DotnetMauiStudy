e737a7c3120d9f94a4aada57817ea9b769afb765
Lucas Carturani <lucas.okea@outlook.com>
2025-01-24 11:47:46 -0300

    Demo de MAUI - XAML e paginas

---
+++ MauiDemo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new MinhaPagina());
+
+            navPage.BarBackground = Colors.Blue;
+            navPage.BarTextColor = Colors.Beige;
+
+            MainPage = new TabbedPageDemo();
+        }
+    }
+}
