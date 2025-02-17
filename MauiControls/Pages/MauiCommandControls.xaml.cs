f7ac9bcc71d18e35e28d94a3f860f598fa7546d0
Lucas Carturani <lucas.okea@outlook.com>
2025-02-17 18:12:17 -0300

    Controle de comandos completo

--- MauiControls/Pages/MauiCommandControls.xaml.cs
+++ MauiControls/Pages/MauiCommandControls.xaml.cs
@@ -12,4 +12,30 @@ public partial class MauiCommandControls : ContentPage
         await lblDemo.RotateTo(360, 2000);
         lblDemo.Rotation = 0;
     }
+
+    private void btnImgDemo_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        RadioButton selectedRadioButton = (RadioButton)sender;
+        if (LblPets != null)
+        {
+            LblPets.Text = $"Selecionou: {selectedRadioButton.Value}";
+            LblPets.FontSize = 20;
+        }
+    }
+
+    private async void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+    {
+        SearchBar searchBar = (SearchBar)sender;
+        await DisplayAlert("Pesquisando...", $"{searchBar.Text}", "Ok");
+    }
+
+    private void SwipeItem_Invoked(object sender, EventArgs e)
+    {
+
+    }
 }
