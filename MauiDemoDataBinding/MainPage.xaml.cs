c23f8cea6d3d8639b15d3c49a931f95926fb5042
Lucas Carturani <lucascarturani@gmail.com>
2025-09-10 00:35:12 -0300

    novo projeto para binding

---
+++ MauiDemoDataBinding/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MauiDemoDataBinding.Models;
+
+namespace MauiDemoDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Produto produto = new()
+            {
+                Nome = "Iphone 5",
+                Preco = 5000.0m,
+                Quantidade = 2
+            };
+
+            Binding produtoBinding = new()
+            {
+                Source = produto,
+                Path = "Nome"
+            };
+            lblNome.SetBinding(Label.TextProperty, produtoBinding);
+        }
+    }
+
+}
---
+++ MauiDemoDataBinding/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDemoDataBinding.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
