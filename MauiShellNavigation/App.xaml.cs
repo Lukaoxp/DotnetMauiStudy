9f2366841397b828cae5cbcd18fc02de01a437a2
Lucas Carturani <lucascarturani@gmail.com>
2025-09-28 20:09:10 -0300

    MauiShellNavigation Project

---
+++ MauiShellNavigation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiShellNavigation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiShellNavigation/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiShellNavigation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
