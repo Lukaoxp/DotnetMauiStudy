7502d584e99e00d0472ecaa540b745ec990b2516
Lucas Carturani <lucascarturani@gmail.com>
2025-09-28 22:07:13 -0300

    Maui Styles Project

---
+++ MauiStyles/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiStyles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiStyles/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiStyles
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
